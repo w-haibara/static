275e9ce955a415a0cedca756568bb0e40d759599
w-haibara <hwhaibarawataru@gmail.com>
2021-02-07 01:29:55 +0900

    add auth handler

--- src/main.go
+++ src/main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
+	"osoba/auth"
 )
 
+func loggingHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("--- logging handler ---")
+		log.Printf("%#v\n", r)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("--- main handler ---")
+	w.Write([]byte("OK"))
+}
+
 func main() {
-	fmt.Println("--- start ---");
-	log.Println("Hello!")
+	auth.Configure(auth.Config{
+		LoginFormURI:  "/login?backTo=/osoba",
+		VerifyKeyFile: "/jwt-secret/secret.key",
+	})
+
+	mainHandler := http.HandlerFunc(mainHandler)
+
+	http.Handle("/", loggingHandler(auth.Handler(mainHandler)))
+	http.ListenAndServe(":8080", nil)
 }
