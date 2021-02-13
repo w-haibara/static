449aee6c40b778abea89cdb2e142989417691c7d
w-haibara <hwhaibarawataru@gmail.com>
2021-02-13 12:30:14 +0900

    refactor: use tempfile

--- src/deploy/deploy.go
+++ src/deploy/deploy.go
@@ -3,14 +3,13 @@ package deploy
 import (
 	"archive/zip"
 	"bytes"
-	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
 )
 
 type Config struct {
@@ -21,8 +20,6 @@ type Config struct {
 }
 
 func (c Config) Deploy() error {
-	c.TmpPath = "/tmp"
-
 	r, err := http.Get(c.ReleaseURL)
 	if err != nil {
 		return err
@@ -39,92 +36,48 @@ func (c Config) Deploy() error {
 		return err
 	}
 
+	tmpRootDir, err := ioutil.TempDir("", "example")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(tmpRootDir)
 	for _, f := range zr.File {
-		fpath := filepath.Join(c.TmpPath, f.Name)
-		if !strings.HasPrefix(fpath, filepath.Clean(c.TmpPath)+string(os.PathSeparator)) {
-			return fmt.Errorf("%s: illegal file path", fpath)
-		}
-
 		if f.FileInfo().IsDir() {
-			if err = os.MkdirAll(fpath, os.ModePerm); err != nil {
+			if err = os.MkdirAll(filepath.Join(tmpRootDir, f.Name), os.ModePerm); err != nil {
 				return err
 			}
 			continue
 		}
 
-		if err := os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
-			return err
-		}
-
-		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-		if err != nil {
-			return err
-		}
-
-		defer outFile.Close()
-
 		rc, err := f.Open()
 		if err != nil {
-			return err
+			log.Println("zip file open err", err)
+			panic(err)
 		}
 		defer rc.Close()
 
-		_, err = io.Copy(outFile, rc)
-		if err != nil {
-			return err
+		buf := new(bytes.Buffer)
+		io.Copy(buf, rc)
+
+		if err := ioutil.WriteFile(filepath.Join(tmpRootDir, f.Name), buf.Bytes(), 0666); err != nil {
+			panic(err)
 		}
 	}
 
-	os.Remove(c.Path)
-
 	distPath := filepath.Join(c.RootPath, c.Path)
 	if err := os.MkdirAll(distPath, os.ModePerm); err != nil {
+		log.Println("MkdirAll err", err)
 		return err
 	}
 	os.Remove(distPath)
 
-	for _, f := range zr.File {
-		fpath := filepath.Join(c.RootPath, f.Name)
-		if !strings.HasPrefix(fpath, filepath.Clean(c.RootPath)+string(os.PathSeparator)) {
-			return fmt.Errorf("%s: illegal file path", fpath)
-		}
-
-		if f.FileInfo().IsDir() {
-			if err = os.MkdirAll(fpath, os.ModePerm); err != nil {
-				return err
-			}
-			continue
-		}
-
-		if err := os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
-			return err
-		}
-
-		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-		if err != nil {
-			return err
-		}
-
-		defer outFile.Close()
-
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer rc.Close()
-
-		_, err = io.Copy(outFile, rc)
-		if err != nil {
-			return err
-		}
-	}
-
 	/*
-		if err := os.Rename(filepath.Join(c.TmpPath, "dist"), distPath); err != nil {
+		if err := os.Rename(filepath.Join(tmpRootDir, "dist"), distPath); err != nil {
 			return err
 		}
 	*/
-	if err := exec.Command("mv", filepath.Join(c.TmpPath, "dist"), distPath).Run(); err != nil {
+
+	if err := exec.Command("mv", filepath.Join(tmpRootDir, "dist"), distPath).Run(); err != nil {
 		return err
 	}
 
