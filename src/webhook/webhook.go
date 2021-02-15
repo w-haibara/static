26f362559f4d92ca782158159aab97c5ec065ac1
w-haibara <hwhaibarawataru@gmail.com>
2021-02-15 11:55:51 +0900

    refactor

--- src/webhook/webhook.go
+++ src/webhook/webhook.go
@@ -1,7 +1,6 @@
 package webhook
 
 import (
-	"errors"
 	"log"
 	"net/http"
 	"os"
@@ -20,24 +19,16 @@ func InitConfigs(configs []Config) []Config {
 
 func (config Config) KeyVerify(w http.ResponseWriter, r *http.Request) error {
 	authHeader := r.Header.Get("Authorization")
-	if !cmpPwd(getToken(config.Path), []byte(authHeader)) {
-		return errors.New("unknown error")
+
+	if err := bcrypt.CompareHashAndPassword(getToken(config.Path), []byte(authHeader)); err != nil {
+		log.Println(err)
+		return err
 	}
-	
+
 	return nil
 }
 
 func getToken(path string) []byte {
 	// TODO: using key value store
 	return []byte(os.Getenv("OSOBA_TOKEN_" + path))
-}
-
-func cmpPwd(hashedPwd []byte, pwd []byte) bool {
-	err := bcrypt.CompareHashAndPassword(hashedPwd, pwd)
-	if err != nil {
-		log.Println(err)
-		return false
-	}
-
-	return true
 }
