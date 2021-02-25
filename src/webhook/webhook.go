0cb70d1c1993768ca84e4deee08787c341e09f85
w-haibara <hwhaibarawataru@gmail.com>
2021-02-25 16:09:27 +0900

    refactor

--- src/webhook/webhook.go
+++ src/webhook/webhook.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 	"osoba/db"
 	"osoba/deploy"
 	"time"
@@ -45,31 +44,30 @@ func Init() {
 
 	pathes := client.NewDB("osoba").NewCollection("pathes", Document{})
 
-	var docs []Document
-	if err := pathes.Read(ctx, bson.M{"path": "/aaa"}, &docs); err != nil {
+	log.Println("delete collection")
+	if err := pathes.Delete(ctx, bson.M{}); err != nil {
 		log.Panic(err)
 	}
-	if len(docs) == 1 {
-		log.Println("DB status OK")
-		return
-	}
 
-	log.Println("insert data")
+	log.Println("set tmp data")
+	// set tmp data
 	if err := pathes.Insert(ctx, []Document{
 		Document{
 			Path:       "/aaa",
 			RootPath:   "/www/html",
 			ReleaseURL: "https://github.com/w-haibara/portfolio/releases/download/v1.0.8/portfolio.zip",
-			Token:      os.Getenv("OSOBA_TMP_TOKEN"),
+			Token:      "$2a$10$sIKCSbHCLnNALUnaeMg1muyPAb4wrM57xJ1sHYmuHhoUtz0u9cqR2",
 		},
 	}); err != nil {
 		log.Panic(err)
 	}
 
+	var docs []Document
 	if err := pathes.Read(ctx, bson.M{}, &docs); err != nil {
 		log.Panic(err)
 	}
 	pp.Println(docs)
+
 	log.Println("DB initializing success")
 }
 
@@ -90,6 +88,8 @@ func FetchInfo(path string) (Info, error) {
 	if err := pathes.Read(ctx, bson.M{"path": path}, &docs); err != nil {
 		return Info{}, err
 	}
+
+	pp.Println(docs)
 
 	if len(docs) != 1 {
 		return Info{}, fmt.Errorf("docs length is invalid: '%#v'\n", docs)
